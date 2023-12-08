000b34947bc7405e7d941aaca0eaf25b9132e1cb
tanakp <tanakpinar1@gmail.com>
2023-12-08 14:23:16 +0300

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,17 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be a positive integer")
+    current_number = 2
+    while len(list) < number_of_primes:
+        is_prime = all(current_number % i != 0 for i in range(2, int(current_number**0.5) + 1))
+
+        if is_prime:
+            list.append(current_number)
+        
+        current_number += 1
+
+
     return list
+
